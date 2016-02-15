In the python script, fixed ds_old input for the first two steps, imposed the original step increment for each new calculation and modified the values for the variable stepper. In generate input files, Gr is rescaled to 1e-4 (please check). In PlotSCurve, the reference curve from the Matlab computations is commented out. This version does go past the 2nd turning point and is equivalent to the older ones where the exp(-Ar) term in the RedbackMaterial .C was commented out. Tested with more threads and it's not working properly yet.

PlotSCurve.py
@@ -70,7 +70,7 @@ def plotSCurve(parameters, logger):
   plt.xlabel('Continuation parameter', fontsize=20)
   plt.ylabel('Norm of the solution', fontsize=20)
 
-  plt.plot(ref_x_values, ref_y_values,'r-x')
+  #plt.plot(ref_x_values, ref_y_values,'r-x')
   plt.hold(True)
   x_values = np.array(lambda_vals)
   y_values = np.array(max_temp_vals)